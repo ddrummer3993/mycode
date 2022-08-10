6e4b857a579ed35535ff8c3c42b0425341a4ee42
ddrummer3993 <ddrummer3993@users.noreply.github.com>
2022-08-10 22:02:28 +0000

    string methods .join() and .split()

---
+++ strings/stringmethod01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#create a small string 
+lilstring = "Alta3 Research offers classes on Python coding"
+newlist = lilstring.split(" ") 
+print(newlist)
+
+#create list of stirngs
+myiplist = [ "192", "168", "0", "12" ]
+singleip = ".".join(myiplist)
+print(singleip)
+
