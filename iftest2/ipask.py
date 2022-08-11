9061119715818c01945994453294d5ee78d90759
ddrummer3993 <ddrummer3993@users.noreply.github.com>
2022-08-11 16:45:42 +0000

    conditionals - if, elif, else logic

---
+++ iftest2/ipask.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+  
+# prompt user for IP address
+ipchk = input('Apply an IP address: ')
+
+# check logic with if statement
+if ipchk:
+    print('Looks like the IP address was set: ' + ipchk)
+else:
+    print('You did not provide input')
+
+
---
+++ iftest2/ipstatic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# prompt user for IP address
+ipchk = input('Apply an IP address: ')
+
+# check logic with if statement
+if ipchk:
+    print('Looks like the IP address was set: ' + ipchk)
+
+
